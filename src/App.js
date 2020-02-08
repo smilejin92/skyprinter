22d7b3d26a31c7e5065eaf70dcb3d7df583a4958
smilejin92 <smilejin92@gmail.com>
2020-02-09 04:25:52 +0900

    Styled header area - installed node-sass - added font weight 400, 500, 700 - created App.scss - removed comments from index.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import logo from "./images/sky-pringer-logo.png";
-import { Icon } from "antd";
+import React, { useState } from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
+import { Icon } from 'antd';
+import logo from './images/sky-pringer-logo.png';
+import './App.scss';
 
 function App() {
   const [selectCountry, setSelectCountry] = useState(false);
@@ -13,63 +15,88 @@ function App() {
     setSelectCountry(false);
   }
 
+  function configCulture() {
+    setSelectCountry(false);
+    // set configuration
+  }
+
   return (
-    <>
+    <BrowserRouter>
       <header className="header">
-        <h1 className="logo">
-          <img src={logo} alt="스카이프린터 로고" />
-        </h1>
+        <div className="wrapper">
+          <h1 className="logo">
+            <Link to="/">
+              <img src={logo} alt="스카이프린터 로고" />
+            </Link>
+          </h1>
+          <nav className="sub-navigation">
+            <h2 className="a11y-hidden">언어 설정 및 로그인 메뉴</h2>
+            <ul className="config-menu">
+              <li className="config-item">
+                <a
+                  className="help"
+                  href="https://help.skyscanner.net/hc/ko?skyCurrency=currency_krw&skyLanguage=lang_ko&skyMarket=kr_skyscanner&_mp=16ff4c302b1108-0342ccc764089b-b383f66-144000-16ff4c302b28f7_1581145007784&preferences=f3fa260e084e455dab664acd265c9a67&traveller_context=f3fa260e-084e-455d-ab66-4acd265c9a67&_ga=2.91829530.163945663.1581136578-799128569.1580359418"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  도움말
+                </a>
+              </li>
+              <li className="config-item">
+                <button
+                  className="btn-select-country"
+                  onClick={displaySelectCountry}
+                >
+                  <div className="config-item-wrapper">
+                    <span className="locale">한국어</span>
+                    <img
+                      className="flag"
+                      src={`https://images.skyscnr.com/images/country/flag/header/${'kr'}.png`}
+                      alt="국기"
+                    />
+                    <span className="market">{'대한민국'}</span>
+                    <span className="currency">{'₩ KRW'}</span>
+                  </div>
+                </button>
+              </li>
+              <li className="config-item">
+                <button className="btn-login">로그인</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <nav className="main-navigation">
           <h2 className="a11y-hidden">검색 메뉴</h2>
           <ul className="search-menu">
-            <li className="search-item active">
-              <button>항공권</button>
-            </li>
             <li className="search-item">
-              <button disabled={true}>호텔</button>
-            </li>
-            <li className="search-item">
-              <button disabled={true}>렌터카</button>
-            </li>
-          </ul>
-        </nav>
-        <nav className="sub-navigation">
-          <h2 className="ally-hidden">언어 설정 및 로그인 메뉴</h2>
-          <ul className="config-menu">
-            <li className="config-item">
-              <a
-                className="help"
-                href="https://help.skyscanner.net/hc/ko?skyCurrency=currency_krw&skyLanguage=lang_ko&skyMarket=kr_skyscanner&_mp=16ff4c302b1108-0342ccc764089b-b383f66-144000-16ff4c302b28f7_1581145007784&preferences=f3fa260e084e455dab664acd265c9a67&traveller_context=f3fa260e-084e-455d-ab66-4acd265c9a67&_ga=2.91829530.163945663.1581136578-799128569.1580359418"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                도움말
-              </a>
-            </li>
-            <li className="config-item">
-              <button
-                className="btn-select-country"
-                onClick={displaySelectCountry}
-              >
-                <div className="config-item-wrapper">
-                  <span className="display-locale">한국어</span>
-                  <span className="display-market">
-                    <img
-                      src={`https://images.skyscnr.com/images/country/flag/header/${"kr"}.png`}
-                      alt="국기"
-                    />
-                    {"대한민국"}
-                  </span>
-                  <span className="display-currency">
-                    {/* code: 3글자, symbol: 기호 */}
-                    {"₩"}
-                    {"KRW"}
-                  </span>
+              <button className="flight active">
+                <div className="button-wrapper">
+                  <svg className="logo" viewBox="0 0 24 24">
+                    <path d="M3.455 8.78a.775.775 0 0 1 .952.224l1.472 1.91 1.816-.267a2.852 2.852 0 0 1 .536-.051h9.862a5.099 5.099 0 0 1 2.438.62.944.944 0 0 1-.072 1.67l-.046.021a5.1 5.1 0 0 1-2.194.497h-4.314l-3.663 5.867A1.546 1.546 0 0 1 8.93 20H7.78l2.007-5.858v-.738h-1.53a2.865 2.865 0 0 1-.589-.062l-1.765-.288-1.509 1.958a.775.775 0 0 1-.929.237.786.786 0 0 1-.419-.984L3.862 12 3.06 9.749a.785.785 0 0 1 .395-.97zM8.929 4a1.545 1.545 0 0 1 1.312.729l3.19 4.985H9.749L7.778 4z" />
+                  </svg>
+                  <span>항공권</span>
                 </div>
               </button>
             </li>
-            <li className="config-item">
-              <button className="btn-login">로그인</button>
+            <li className="search-item">
+              <button className="hotel" disabled={true}>
+                <div className="button-wrapper">
+                  <svg className="logo" viewBox="0 0 24 24">
+                    <path d="M3.827 6.65c.428 0 .81.15.855.342l.005.04v3.196a2.356 2.356 0 0 0 2.16 2.438l.11.004 13.878.02c.371 0 0 4.59 0 4.971a.67.67 0 0 1-1.32.184l-.014-.07-.32-1.948H4.687l-.352 1.949a.67.67 0 0 1-1.331-.045L3 17.661V7.031c0-.21.37-.381.827-.381zm14.674 2.228a1.887 1.887 0 0 1 1.936 1.834v.087a.891.891 0 0 1-.914.865h-8.859a.891.891 0 0 1-.914-.865v-.087a1.887 1.887 0 0 1 1.936-1.834zM7.5 7.764a1.672 1.672 0 1 1 0 3.343 1.672 1.672 0 1 1 0-3.343z" />
+                  </svg>
+                  <span>호텔</span>
+                </div>
+              </button>
+            </li>
+            <li className="search-item">
+              <button className="car" disabled={true}>
+                <div className="button-wrapper">
+                  <svg className="logo" viewBox="0 0 24 24">
+                    <path d="M6 14a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 6 14zm12 0a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 18 14zM9.854 7.5a8.552 8.552 0 0 1 2.872.45 15.534 15.534 0 0 1 3.602 2.466 13.077 13.077 0 0 1 4.842 1.1c.54.28.691 1.294.816 2.14.14.946-.812 1.406-1.466 1.638l-.037.017A2.5 2.5 0 1 0 15.54 16H8.45a2.5 2.5 0 1 0-4.95-.5q0 .09.006.183c-.161-.043-.249-.076-.293-.09a1.815 1.815 0 0 1-1.185-2.101v-.003l.503-2.868h10.923c.601 0 .842-.595.271-.981a5.71 5.71 0 0 0-1.326-.72 7.612 7.612 0 0 0-2.546-.394 13.162 13.162 0 0 0-2.681.3c-1.157.243-2.692.69-3.658 1.002a.457.457 0 0 1-.552-.24l-.022-.054-.004-.01a.478.478 0 0 1 .277-.608 27.517 27.517 0 0 1 3.76-1.095 14.094 14.094 0 0 1 2.88-.32z" />
+                  </svg>
+                  <span>렌터카</span>
+                </div>
+              </button>
             </li>
           </ul>
         </nav>
@@ -77,7 +104,9 @@ function App() {
       <main className="main-content">
         <div className="search-area-wrapper">
           <section className="search-area">
-            <h2>검색 영역</h2>
+            <h2 className="a11y-hidden">검색 영역</h2>
+            <h3 className="title">어디로 떠나볼까요?</h3>
+            <div className="travel-info">정보 입력</div>
           </section>
           <div className="search-area-arrow-wrapper">
             <svg viewBox="0 0 14832 55" className="search-area-arrow">
@@ -90,40 +119,65 @@ function App() {
       {selectCountry && (
         <div className="select-country-wrapper">
           <article className="select-country">
-            <h2 className="select-country-title">국가 설정</h2>
-            <button
-              className="btn-close-select-country"
-              onClick={hideSelectCountry}
-            >
-              <Icon type="close" />
-            </button>
-            <div className="select-locale">
-              <label htmlFor="locale">언어</label>
-              <select id="locale">
-                <option>한국어 (대한민국)</option>
-              </select>
-            </div>
-            <div className="select-market">
-              <label htmlFor="market">국가/지역</label>
-              <select id="market">{/* get list of markets */}</select>
-            </div>
-            <div className="select-currency">
-              <label htmlFor="currency">통화</label>
-              <select id="currency">{/* get list of currencies */}</select>
-            </div>
-            <div className="btn-group">
-              <button className="btn-save-select-country">저장</button>
-              <button
-                className="btn-cancel-select-country"
-                onClick={hideSelectCountry}
-              >
-                취소
+            <div className="header-wrapper">
+              <h2 className="title">국가 설정</h2>
+              <button className="btn-close" onClick={hideSelectCountry}>
+                <Icon type="close" />
               </button>
+            </div>
+            <div className="content-wrapper">
+              <div className="select-wrapper">
+                <label htmlFor="locale">
+                  <svg>
+                    <path d="M21 5c-.2-.3-.6-.4-.6-.4-.8-.4-4.6-.6-8.5-.6s-7.7.2-8.5.6c0 0-.4.1-.6.4-.6.6-.9 3-.9 5.5s.3 4.9.9 5.5c.2.3.6.4.6.4.5.2 1.8.4 3.6.5v2.6c0 .3.3.6.7.5 1-.3 2.7-1.2 4-3h.3c3.9 0 7.7-.2 8.5-.6 0 0 .4-.1.6-.4.6-.6.9-3.1.9-5.5s-.4-4.9-1-5.5zM6.4 12c-.8 0-1.5-.7-1.5-1.5S5.6 9 6.4 9s1.5.7 1.5 1.5S7.3 12 6.4 12zm6.6-.4c-.3.3-.6.4-1.1.4-.8 0-1.5-.7-1.5-1.5 0-.6.4-1.1.9-1.3.2-.1.4-.2.6-.2.8 0 1.5.7 1.5 1.5 0 .4-.1.8-.4 1.1zm4.4.4c-.8 0-1.5-.7-1.5-1.5S16.5 9 17.4 9s1.5.7 1.5 1.5-.7 1.5-1.5 1.5z"></path>
+                  </svg>
+                  언어
+                </label>
+                <select id="locale">
+                  <option>한국어 (대한민국)</option>
+                </select>
+              </div>
+              <div className="select-wrapper">
+                <label htmlFor="market">
+                  <svg>
+                    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm6.9 5.9h-2.3c-.3-1.2-.6-2.2-1.1-3.1 1.4.7 2.6 1.8 3.4 3.1zM15 12c0 .7 0 1.4-.1 2H9.1c-.1-.6-.1-1.3-.1-2s0-1.4.1-2.1h5.8c.1.7.1 1.4.1 2.1zm-3 8c-.7 0-1.9-1.5-2.5-4h5.1c-.7 2.5-1.9 4-2.6 4zM9.5 7.9C10.1 5.4 11.3 4 12 4s1.9 1.4 2.5 3.9h-5zm-1-3.1c-.4.9-.8 1.9-1.1 3.1H5.1c.8-1.3 2-2.4 3.4-3.1zM4.3 9.9h2.8C7 10.6 7 11.3 7 12s0 1.3.1 2H4.3c-.2-.6-.3-1.3-.3-2s.1-1.4.3-2.1zm.8 6.1h2.3c.3 1.2.6 2.3 1.1 3.2-1.4-.7-2.6-1.8-3.4-3.2zm10.4 3.2c.5-.9.8-2 1.1-3.2h2.3c-.8 1.4-2 2.5-3.4 3.2zm4.2-5.2h-2.8c.1-.6.1-1.3.1-2s0-1.4-.1-2.1h2.8c.2.7.3 1.4.3 2.1 0 .7-.1 1.4-.3 2z"></path>
+                  </svg>
+                  국가/지역
+                </label>
+                <p>
+                  국가를 선택하면 지역별 특가 상품 및 정보를 받아보실 수
+                  있습니다.
+                </p>
+                <select id="market">
+                  {/* get list of markets */}
+                  <option>대한민국</option>
+                </select>
+              </div>
+              <div className="select-wrapper">
+                <label htmlFor="currency">
+                  <svg>
+                    <path d="M17 9.3v.2c0 .7-.1 1.3-.3 1.9 1.3.7 2.3 2 2.3 3.6 0 2.2-1.8 4-4 4-1.6 0-2.9-.9-3.6-2.3-.6.2-1.3.3-1.9.3h-.2c.8 2.3 3 4 5.7 4 3.3 0 6-2.7 6-6 0-2.6-1.7-4.8-4-5.7zm-1 .2C16 5.9 13.1 3 9.5 3S3 5.9 3 9.5 5.9 16 9.5 16 16 13.1 16 9.5zm-9.4 2.6l.9-1.2c.7.4 1.2.6 1.7.6.6 0 .9-.2.9-.6 0-.8-3.2-1-3.2-2.8 0-1.1.7-1.8 1.9-2V5h1.4v1.1c.8.1 1.4.4 1.9.9L11 8c-.4-.3-.8-.5-1.3-.5s-.8.1-.8.5c0 .8 3.2.8 3.2 2.7 0 1-.6 1.8-1.9 2.1V14H8.8v-1c-.7-.1-1.6-.4-2.2-.9z"></path>
+                  </svg>
+                  통화
+                </label>
+                <select id="currency">
+                  {/* get list of currencies */}
+                  <option>KRW - ₩</option>
+                </select>
+              </div>
+              <div className="btn-group select-wrapper">
+                <button className="btn-save" onClick={configCulture}>
+                  저장
+                </button>
+                <button className="btn-cancel" onClick={hideSelectCountry}>
+                  취소
+                </button>
+              </div>
             </div>
           </article>
         </div>
       )}
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
