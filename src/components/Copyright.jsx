a9d927c1ba7c1b92ce303899bcb468381d9dc28e
HYEOK999 <iasg2004@naver.com>
2020-02-12 16:35:58 +0900

    feat : Update the CSS at <small>

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSmall = styled.small`
+  display: block;
+  width: 1048px;
+  margin: 1.2rem auto;
+  color: #444560;
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+  font-weight: 400;
+  letter-spacing: normal;
+`;
+
+function Copyright() {
+  return <StyledSmall>&copy; Skyscanner Ltd 2002-2020</StyledSmall>;
+}
+
+export default Copyright;
