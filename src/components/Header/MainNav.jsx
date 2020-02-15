5f1dbf141c88e2909b1071af1969433726ba3ad7
HYEOK999 <iasg2004@naver.com>
2020-02-15 18:11:28 +0900

    feat : Change the CSS

--- src/components/Header/MainNav.jsx
+++ src/components/Header/MainNav.jsx
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 import { HiddenHeader, FlexList, FlexWrapper } from '../../styles';
 
 const SearchMenu = styled(FlexList)`
-  width: 28.3rem;
+  width: 26.6rem;
   height: 3.6rem;
 `;
 
