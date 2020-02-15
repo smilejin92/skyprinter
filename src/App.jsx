f7a6db95e6cbcfd95d5f5f5733c7d623f79937bf
HYEOK999 <iasg2004@naver.com>
2020-02-15 18:09:44 +0900

    feat : Remove the routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DatePicker from './components/DatePicker';
 import SearchBox from './components/Main/SearchBox';
-import CabinClassAndPessenger from './components/CabinClassAndPessenger';
+// import CabinClassAndPessenger from './components/CabinClassAndPessenger';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/datePicker" component={DatePicker} />
         <Route exact path="/searchBox" component={SearchBox} />
-        <Route exact path="/cabinClass" component={CabinClassAndPessenger} />
+        {/* <Route exact path="/cabinClass" component={CabinClassAndPessenger} /> */}
         <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
