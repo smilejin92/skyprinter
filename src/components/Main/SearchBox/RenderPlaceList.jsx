7fa4709c066a5c708c0114a0a69c8ec26f40ce9e
HYEOK999 <iasg2004@naver.com>
2020-02-17 00:13:58 +0900

    feat : Move the ListSection StyleComponent

--- src/components/Main/SearchBox/RenderPlaceList.jsx
+++ src/components/Main/SearchBox/RenderPlaceList.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
-import { ListSection } from './BoundSearchBox';
 import ParseWord from './ParseWord';
+import styled from 'styled-components';
 import uuid from 'uuid';
+
+const ListSection = styled.section`
+  display: flex;
+  flex-direction: row;
+  color: #111236;
+  padding: 1.2rem 1.8rem;
+  padding-left: ${props => (props.hasCountry ? '30px' : '18px')};
+  svg {
+    fill: #b2b2bf;
+    overflow: hidden;
+    margin-top: -2px;
+    margin-right: 0.96rem;
+    vertical-align: top;
+  }
+  div {
+    width: 100%;
+  }
+  span {
+    margin: 0;
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    font-weight: 400;
+    letter-spacing: normal;
+  }
+  small {
+    display: block;
+    font-size: 1.2rem;
+    line-height: 1.8rem;
+    font-weight: 400;
+    letter-spacing: normal;
+  }
+`;
 
 const RenderPlaceList = React.memo(({ place, suggestion, hasCountry }) => {
   const result = {};
@@ -84,9 +116,7 @@ const RenderPlaceList = React.memo(({ place, suggestion, hasCountry }) => {
     }
     default: {
       return (
-        <ListSection
-          style={hasCountry ? { paddingLeft: '30px' } : { paddingLeft: '18px' }}
-        >
+        <ListSection hasCountry={hasCountry}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
             <path d="M3.455 8.78a.775.775 0 0 1 .952.224l1.472 1.91 1.816-.267a2.852 2.852 0 0 1 .536-.051h9.862a5.099 5.099 0 0 1 2.438.62.944.944 0 0 1-.072 1.67l-.046.021a5.1 5.1 0 0 1-2.194.497h-4.314l-3.663 5.867A1.546 1.546 0 0 1 8.93 20H7.78l2.007-5.858v-.738h-1.53a2.865 2.865 0 0 1-.589-.062l-1.765-.288-1.509 1.958a.775.775 0 0 1-.929.237.786.786 0 0 1-.419-.984L3.862 12 3.06 9.749a.785.785 0 0 1 .395-.97zM8.929 4a1.545 1.545 0 0 1 1.312.729l3.19 4.985H9.749L7.778 4z"></path>
           </svg>
