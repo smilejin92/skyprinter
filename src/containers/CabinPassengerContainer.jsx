1b1b2bf398ca02488b32398f196c08a00e40b4a6
HYEOK999 <iasg2004@naver.com>
2020-02-21 22:38:48 +0900

    feat : Added the code(error state)

--- src/containers/CabinPassengerContainer.jsx
+++ src/containers/CabinPassengerContainer.jsx
@@ -1,16 +1,22 @@
 // import React from 'react';
 import { displayModal } from '../redux/modules/display';
 import { connect } from 'react-redux';
+import { setError } from '../redux/modules/error';
 import ClassGradeButton from '../components/Main/CabinClassPassenger/index';
 
 const mapStateToProps = state => ({
   visible: state.display.passengerInfo,
   passengerInfo: state.passenger,
+  errors: state.error.errors,
+  errorOccurred: state.error.errorOccurred,
 });
 
 const mapDispatchToProps = dispatch => ({
   displayModal: () => {
     dispatch(displayModal('passengerInfo'));
+  },
+  setError: errors => {
+    dispatch(setError(errors));
   },
 });
 
