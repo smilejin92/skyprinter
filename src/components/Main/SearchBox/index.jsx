1429eabb9f74bbf839f2dd8edf24ffcfbf3c037a
HYEOK999 <iasg2004@naver.com>
2020-02-21 15:34:08 +0900

    fix : Changed the state(added local state)

--- src/components/Main/SearchBox/index.jsx
+++ src/components/Main/SearchBox/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import BoundSearchBox from './BoundSearchBox';
 import BoundChangeBox from './BoundChangeBox';
@@ -16,6 +16,11 @@ const SearchBoxWrapper = styled.div`
 
 const SearchBox = React.memo(
   ({ places, setPlace, switchPlaces, errors, errorOccurred, setError }) => {
+    const [bound, setBound] = useState({
+      inBoundName: places.inBoundName,
+      outBoundName: places.outBoundName,
+    });
+
     const selectBound = (PlaceId, PlaceName, type) => {
       setPlace({ PlaceId, PlaceName, type });
       // if (errorOccurred) {
@@ -37,21 +42,31 @@ const SearchBox = React.memo(
       // }
     };
 
+    const changeBound = () => {
+      setBound({
+        inBoundName: places.outBoundName,
+        outBoundName: places.inBoundName,
+      });
+      switchPlaces();
+    };
+
     const { pathname } = useLocation();
 
     return (
       <SearchBoxWrapper page={pathname}>
         <BoundSearchBox
           header={'출발지'}
-          bound={places}
-          selectBound={selectBound}
+          bound={bound} // 지역 state
+          setBound={setBound} // 지역 state Update
+          selectBound={selectBound} // 전역 state Update
           type="inBound"
         />
-        <BoundChangeBox changeBound={switchPlaces} />
+        <BoundChangeBox changeBound={changeBound} />
         <BoundSearchBox
           header={'도착지'}
-          bound={places}
-          selectBound={selectBound}
+          bound={bound} // 지역 state
+          setBound={setBound} // 지역 state Update
+          selectBound={selectBound} // 전역 state Update
           type="outBound"
           borderRadius="none"
         />
