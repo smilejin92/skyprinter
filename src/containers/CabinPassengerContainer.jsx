7b62a43d734abe1ba552a138797fd3b794abed44
HYEOK999 <iasg2004@naver.com>
2020-02-22 16:11:20 +0900

    feat : Removed the setError props (container)

--- src/containers/CabinPassengerContainer.jsx
+++ src/containers/CabinPassengerContainer.jsx
@@ -1,7 +1,6 @@
 // import React from 'react';
 import { displayModal } from '../redux/modules/display';
 import { connect } from 'react-redux';
-import { setError } from '../redux/modules/error';
 import ClassGradeButton from '../components/Main/CabinClassPassenger/index';
 
 const mapStateToProps = state => ({
@@ -14,9 +13,6 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   displayModal: () => {
     dispatch(displayModal('passengerInfo'));
-  },
-  setError: errors => {
-    dispatch(setError(errors));
   },
 });
 
