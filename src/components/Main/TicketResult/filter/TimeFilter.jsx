b7fdeaf43eb674abde49c490d85218ab9b8b173c
HYEOK999 <iasg2004@naver.com>
2020-02-23 12:02:21 +0900

    feat : Updated AirportFilter CSS & MarkUp

--- src/components/Main/TicketResult/filter/TimeFilter.jsx
+++ src/components/Main/TicketResult/filter/TimeFilter.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
+import {
+  FilterWrapperDl,
+  FilterWrapperButton,
+  StyleSlider,
+  TimeHeader,
+  TimeContent,
+  InBoundTimeDiv,
+  OutBoundTimeDiv,
+} from '../../../styles/Filter.style';
 
 const TimeFilter = props => {
   return (
-    <dl>
+    <FilterWrapperDl>
       <div>
         <dt>
-          <button>
+          <FilterWrapperButton>
             <span>출발 시간대 설정</span>
-            <svg></svg>
-          </button>
+            <svg viewBox="0 0 24 24" width="18" height="18">
+              <path d="M12 17.5l-7.2-6.4c-.6-.5-.7-1.5-.1-2.1.5-.6 1.5-.7 2.1-.1l5.2 4.6 5.2-4.6c.6-.6 1.6-.5 2.1.1s.5 1.6-.1 2.1L12 17.5z"></path>
+            </svg>
+          </FilterWrapperButton>
         </dt>
         <dd>
           <div>
-            <div>
-              <b>가는날 출발 시간</b>
-              <p>오전 12:00 - 오후 11:59</p>
-              <div>슬라이더</div>
-            </div>
-            <div>
-              <b>오는날 출발 시간</b>
-              <p>오전 12:00 - 오후 11:59</p>
-              <div>슬라이더</div>
-            </div>
+            <OutBoundTimeDiv>
+              <TimeHeader>가는날 출발 시간</TimeHeader>
+              <TimeContent>오전 12:00 - 오후 11:59</TimeContent>
+              <div>
+                <StyleSlider range defaultValue={[20, 50]} />
+              </div>
+            </OutBoundTimeDiv>
+            <InBoundTimeDiv>
+              <TimeHeader>오는날 출발 시간</TimeHeader>
+              <TimeContent>오전 12:00 - 오후 11:59</TimeContent>
+              <div>
+                <StyleSlider range defaultValue={[20, 50]} />
+              </div>
+            </InBoundTimeDiv>
           </div>
         </dd>
       </div>
-    </dl>
+    </FilterWrapperDl>
   );
 };
 
