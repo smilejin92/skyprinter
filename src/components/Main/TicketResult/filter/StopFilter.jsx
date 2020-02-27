9645d32cc2e2040be62f70e24282619ff74b05a8
HYEOK999 <iasg2004@naver.com>
2020-02-27 22:59:39 +0900

    feat : Added the Code

--- src/components/Main/TicketResult/filter/StopFilter.jsx
+++ src/components/Main/TicketResult/filter/StopFilter.jsx
@@ -11,7 +11,7 @@ import {
   FilterDropDiv,
   OptionContent,
   OptionHeader,
-  StyleCheckBox
+  StyleCheckBox,
 } from '../../../styles/Filter.style';
 import uuid from 'uuid';
 
@@ -50,7 +50,7 @@ const StopFilter = React.memo(({ session, setFilter }) => {
 
         const ticket = {
           PricingOptions,
-          OutboundLeg
+          OutboundLeg,
         };
 
         // get Inbound Leg (왕복이라면)
@@ -121,10 +121,10 @@ const StopFilter = React.memo(({ session, setFilter }) => {
           price:
             DirectStopList.length >= 1
               ? `₩ ${numberWithCommas(
-                  Math.floor(DirectStopList[0].PricingOptions[0].Price)
+                  Math.floor(DirectStopList[0].PricingOptions[0].Price),
                 )}`
               : '없음',
-          disabled: DirectStopList.length === 0 ? true : false
+          disabled: DirectStopList.length === 0 ? true : false,
         },
         {
           id: '1회 경유',
@@ -132,22 +132,22 @@ const StopFilter = React.memo(({ session, setFilter }) => {
           price:
             OneOverStopList.length >= 1
               ? `₩ ${numberWithCommas(
-                  Math.floor(OneOverStopList[0].PricingOptions[0].Price)
+                  Math.floor(OneOverStopList[0].PricingOptions[0].Price),
                 )}`
               : '없음',
-          disabled: OneOverStopList.length === 0 ? true : false
+          disabled: OneOverStopList.length === 0 ? true : false,
         },
         {
           id: '모두',
           checked: session.filterOption.stops === undefined ? true : false,
           price: allStopPrice(),
-          disabled: false
-        }
+          disabled: false,
+        },
       ];
 
       return stops;
     },
-    [session.filterOption.stops]
+    [session.filterOption.stops],
   );
 
   useEffect(() => {
@@ -212,13 +212,14 @@ const StopFilter = React.memo(({ session, setFilter }) => {
 });
 
 const mapStateToProps = state => ({
-  session: state.session
+  session: state.session,
 });
 
 const mapDispatchToProps = dispatch => ({
   setFilter: filterOption => {
     dispatch(setFilterOption(filterOption));
     dispatch(pollSession(true));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(StopFilter);
