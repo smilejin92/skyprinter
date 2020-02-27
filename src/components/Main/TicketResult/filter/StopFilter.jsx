c694e502d4c46c026bbfc4c896900b5f996f1af1
HYEOK999 <iasg2004@naver.com>
2020-02-28 07:34:10 +0900

    feat : Removed the console.log

--- src/components/Main/TicketResult/filter/StopFilter.jsx
+++ src/components/Main/TicketResult/filter/StopFilter.jsx
@@ -155,7 +155,6 @@ const StopFilter = React.memo(({ session, setFilter }) => {
   }, [getStops, session.allResult]);
 
   const onChange = stopList => {
-    console.log(stopList);
     if (stopList.id === '직항') {
       setFilter({ ...session.filterOption, stops: 0 });
     }
