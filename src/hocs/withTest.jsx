31d3573cb648ef34b15fa8d4aa4e36a8f52b8a74
smilejin92 <smilejin92@gmail.com>
2020-03-05 01:32:48 +0900

    added useEffect in withTest.jsx

--- src/hocs/withTest.jsx
+++ src/hocs/withTest.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import qs from 'query-string';
 import { useSelector, useDispatch } from 'react-redux';
 import { createSession } from '../redux/modules/session';
@@ -12,64 +12,81 @@ function withTest(Component) {
     const culture = useSelector(state => state.culture);
     const dispatch = useDispatch();
 
-    const urlQuery = qs.parse(props.location.search);
-    console.log('url쿼리', urlQuery);
-    const places = {
-      inBoundId: props.match.params.originId,
-      inBoundName: urlQuery.originPlaceName,
-      outBoundId: props.match.params.destinationId,
-      outBoundName: urlQuery.destinationPlaceName,
-    };
+    useEffect(() => {
+      const urlQuery = qs.parse(props.location.search);
+      console.log('url쿼리', urlQuery);
+      const places = {
+        inBoundId: props.match.params.originId,
+        inBoundName: urlQuery.originPlaceName,
+        outBoundId: props.match.params.destinationId,
+        outBoundName: urlQuery.destinationPlaceName
+      };
 
-    const datepicker = {
-      tripType: urlQuery.tripType,
-      outboundDate: new Date(props.match.params.outboundDate),
-      inboundDate: urlQuery.inboundDate ? new Date(urlQuery.inboundDate) : null,
-      prevInboundDate: null,
-    };
+      const datepicker = {
+        tripType: urlQuery.tripType,
+        outboundDate: new Date(props.match.params.outboundDate),
+        inboundDate: urlQuery.inboundDate
+          ? new Date(urlQuery.inboundDate)
+          : null,
+        prevInboundDate: null
+      };
 
-    const children = urlQuery.children
-      ? urlQuery.childrenAge
-          .split('|')
-          .map((c, i) => ({ id: i, age: c, type: c >= 2 ? 'child' : 'infant' }))
-      : [];
+      const children = urlQuery.children
+        ? urlQuery.childrenAge
+            .split('|')
+            .map((c, i) => ({
+              id: i,
+              age: c,
+              type: c >= 2 ? 'child' : 'infant'
+            }))
+        : [];
 
-    const passenger = {
-      cabinClass: urlQuery.cabinclass,
-      adults: +urlQuery.adults,
-      children,
-      //urlQuery.children, urlQuery.infants
-    };
-    // Query String
-    const allInfo = {
-      culture: culture,
-      places: places,
-      passenger: passenger,
-      datepicker: datepicker,
-    };
+      const passenger = {
+        cabinClass: urlQuery.cabinclass,
+        adults: +urlQuery.adults,
+        children
+        //urlQuery.children, urlQuery.infants
+      };
+      // Query String
+      const allInfo = {
+        culture: culture,
+        places: places,
+        passenger: passenger,
+        datepicker: datepicker
+      };
 
-    // createSession
-    dispatch(createSession(allInfo));
+      // createSession
+      dispatch(createSession(allInfo));
 
-    // reset ReduxStore
-    if (storePlaces && storePlaces.inBoundId.length === 0) {
-      dispatch(
-        setPlace({
-          PlaceId: places.inBoundId,
-          PlaceName: places.inBoundName,
-          type: 'inBound',
-        }),
-      );
-      dispatch(
-        setPlace({
-          PlaceId: places.outBoundId,
-          PlaceName: places.outBoundName,
-          type: 'outBound',
-        }),
-      );
-      dispatch(resetDate({ ...datepicker }));
-      dispatch(resetPassenger({ ...passenger }));
-    }
+      // reset ReduxStore
+      if (storePlaces && storePlaces.inBoundId.length === 0) {
+        dispatch(
+          setPlace({
+            PlaceId: places.inBoundId,
+            PlaceName: places.inBoundName,
+            type: 'inBound'
+          })
+        );
+        dispatch(
+          setPlace({
+            PlaceId: places.outBoundId,
+            PlaceName: places.outBoundName,
+            type: 'outBound'
+          })
+        );
+        dispatch(resetDate({ ...datepicker }));
+        dispatch(resetPassenger({ ...passenger }));
+      }
+    }, [
+      culture,
+      dispatch,
+      props.location.search,
+      props.match.params.destinationId,
+      props.match.params.originId,
+      props.match.params.outboundDate,
+      storePlaces
+    ]);
+
     return <Component {...props} />;
     // }
   }
